617f2cefea08e1782106518f45ceb2bc2947b4ef
leoadvance <leo_advance@163.com>
2016-10-18 11:30:00 +0800

    Init

---
+++ initial/lib/gcc/arm-openwrt-linux-uclibcgnueabi/4.8.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/leo/myGit/tina/build_dir/toolchain-arm_cortex-a8+neon_gcc-4.8-linaro_uClibc-0.9.33.2_eabi/gcc-linaro-4.8-2014.04/configure --with-bugurl=https://dev.openwrt.org/ --with-pkgversion='OpenWrt/Linaro GCC 4.8-2014.04 unknown' --prefix=/home/leo/myGit/tina/staging_dir/toolchain-arm_cortex-a8+neon_gcc-4.8-linaro_uClibc-0.9.33.2_eabi --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=arm-openwrt-linux-uclibcgnueabi --with-gnu-ld --enable-target-optspace --disable-libmudflap --disable-multilib --disable-nls --with-host-libstdcxx=-lstdc++ --with-gmp=/home/leo/myGit/tina/staging_dir/host --with-mpfr=/home/leo/myGit/tina/staging_dir/host --disable-decimal-float --with-mpc=/home/leo/myGit/tina/staging_dir/host --disable-libssp --disable-__cxa_atexit --with-arch=armv7-a --with-float=hard --with-newlib --with-sysroot=/home/leo/myGit/tina/staging_dir/toolchain-arm_cortex-a8+neon_gcc-4.8-linaro_uClibc-0.9.33.2_eabi --enable-languages=c --disable-shared --disable-threads";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "arch", "armv7-a" }, { "float", "hard" }, { "tls", "gnu" } };
